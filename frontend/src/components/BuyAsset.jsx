8a4816a8ad54789a3c277e73e7eb45ca4e5832d5
k0shreya <kokkiralashreyarao30@gmail.com>
2026-02-05 06:50:36 +0000

    fixed axios

--- frontend/src/components/BuyAsset.jsx
+++ frontend/src/components/BuyAsset.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { buyAsset } from "../api/portfolioApi";
 import { searchYahooSymbols } from "../api/yahooApi";
 import "./BuyAsset.css";
